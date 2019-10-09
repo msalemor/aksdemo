b89210fce524c2020e939e128b6a9e9e26a29f09
Alex Morales <am8850@hotmail.com>
2019-10-09 17:16:25 -0400

    - Update build-images script to build and push images to ACR - Update docker-compose to fix typos

--- build-images.sh
+++ build-images.sh
@@ -3,11 +3,25 @@
 ver=$(date +"%Y%m%d%H%M")
 
 # Build the images
-docker build -t alemor/coreapi:$ver -f Dockerfile-CoreAPI .
-docker build -t alemor/apigateway:$ver -f Dockerfile-APIGateway .
-docker build -t alemor/frontend -f:$ver Dockerfile-FrontEnd .
+docker build -t alemor/coreapi -f Dockerfile-CoreAPI .
+docker build -t alemor/apigateway -f Dockerfile-APIGateway .
+docker build -t alemor/frontend -f Dockerfile-FrontEnd .
 
 # Run the images in docker
 #docker run --rm alemor/coreapi
 #docker run --rm -e CoreContactsAPIURI=http://coreapi/api/contacts alemor/apigateway
 #docker run --rm -e APIGatewayCustomersURI=http://apigateway/api/customers -p 8090:80 alemor/frontend
+
+docker login alemoracr.azurecr.io
+# Provide User name and password from admin ACR
+
+
+docker tag alemor/coreapi alemoracr.azurecr.io/alemor/coreapi
+docker push alemoracr.azurecr.io/alemor/coreapi
+
+docker tag alemor/apigateway alemoracr.azurecr.io/alemor/apigateway
+docker push alemoracr.azurecr.io/alemor/apigateway
+
+docker tag alemor/frontend alemoracr.azurecr.io/alemor/frontend
+docker push alemoracr.azurecr.io/alemor/frontend
+
