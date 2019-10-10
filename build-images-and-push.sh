85682795ddd83aab0fc5e10cd922c7b76783c493
Alex Morales <am8850@hotmail.com>
2019-10-10 10:39:53 -0400

    - Added more logging to the build step - Moved build files to root folder

--- build-images-and-push.sh
+++ build-images-and-push.sh
@@ -1,11 +1,15 @@
-﻿
+﻿#/bin/bash
+
 # Always a good practice to use a version
-ver=$(date +"%Y%m%d%H%M")
+#ver=$(date +"%Y%m%d%H%M")
 #docker build -t alemor/coreapi:$ver -f Dockerfile-CoreAPI .
 
 # Build the images
+echo "Building CoreAPI"
 docker build -t alemor/coreapi -f Dockerfile-CoreAPI .
+echo "Building APIGateway"
 docker build -t alemor/apigateway -f Dockerfile-APIGateway .
+echo "Building FrontEnd"
 docker build -t alemor/frontend -f Dockerfile-FrontEnd .
 
 # Run the images in docker
@@ -13,16 +17,18 @@ docker build -t alemor/frontend -f Dockerfile-FrontEnd .
 #docker run --rm -e CoreContactsAPIURI=http://coreapi/api/contacts alemor/apigateway
 #docker run --rm -e APIGatewayCustomersURI=http://apigateway/api/customers -p 8090:80 alemor/frontend
 
+echo "Login into ACR"
 docker login alemoracr.azurecr.io
 # Provide User name and password from admin ACR
 
-
+echo "Tag and Push CoreAPI"
 docker tag alemor/coreapi alemoracr.azurecr.io/alemor/coreapi
 docker push alemoracr.azurecr.io/alemor/coreapi
 
+echo "Tag and Push APIGateway"
 docker tag alemor/apigateway alemoracr.azurecr.io/alemor/apigateway
 docker push alemoracr.azurecr.io/alemor/apigateway
 
+echo "Tag and Push FrontEnd"
 docker tag alemor/frontend alemoracr.azurecr.io/alemor/frontend
 docker push alemoracr.azurecr.io/alemor/frontend
-
